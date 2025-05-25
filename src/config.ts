33b03636ed619b95a7a233ec1545dc348507339d
maybeizen <briarchamberlin1@gmail.com>
2025-05-25 19:02:36 -0400

    chore: update staff role IDs and ticket category IDs in configuration

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,19 @@ import type { BotConfig } from "../types/global";
 
 const config: BotConfig = {
   staff: {
-    staffRoleIds: ["1288652975413133407", "1288652951992139919"],
+    staffRoleIds: [
+      "1069001432805679184",
+      "1004920336120283260",
+      "1099140463321165896",
+      "1004920336120283262",
+    ], // support team, trainee, admin, manager
   },
   tickets: {
-    parentCategoryId: "1288653678806569032",
-    closedCategoryId: "1319852402567811102",
+    parentCategoryId: "1064379366496288818",
+    closedCategoryId: "1319854826347171961",
   },
   autorole: {
-    communityRoleId: "1288653069093175387",
+    communityRoleId: "1004920336120283257",
   },
 };
 
